b115949fc9eb3e0e4cd8ee6f63a74f5851b2a8a4
vermagaurav24 <gaurav123cse@gmail.com>
2025-08-02 10:00:30 +0530

    Frist

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A crome plugin to Analyse comments',
+    author='Gopal Maurya',
+    license='MIT',
+)
